442003d94f851961a76afdf96bf0fc50a13d377b
amin2783 <64882854+amin2783@users.noreply.github.com>
2021-04-18 21:32:26 +0600

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,38 +1,35 @@
-import os
-import sys
-import pathlib
+import os, sys, pathlib
 
 alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 for char in alphabet:
 
-    for path in pathlib.Path(r"YOUR_PATH_HERE").iterdir():
+    for path in pathlib.Path(r"YOUR_PATH_HERE").iterdir():  #Path should be like this "C:\Users\username\Desktop\New folder\\"
+                                                            #Keep the quotes, and note the double \\ at the end
         if path.is_file():
-            if "desktop.ini" in str(path):
-                break
-
-
-            fin = open(path, "rt", encoding= "ANSI")
-
-            data = fin.read()
-
-            if f"{char}:\\" in data:
-
-                data = data.replace(f"{char}:\\", f"/home/user/media/{char}:/")
-                fin.close()
-
-                fin = open(path, "wt", encoding= "ANSI")
-                fin.write(data)
-                fin.close()
+            if ".fastresume" in str(path):
 
                 fin = open(path, "rt", encoding= "ANSI")
-                data = fin.read()
-                data = data.replace("\\", "/")
-                fin.close()
 
-                fin = open(path, "wt", encoding= "ANSI")
-                fin.write(data)
-                fin.close()
+                data = fin.read()
+
+                if f"{char}:\\" in data:
+
+                    data = data.replace(f"{char}:\\", f"/home/user/media/{char}:/")
+                    fin.close()
+
+                    fin = open(path, "wt", encoding= "ANSI")
+                    fin.write(data)
+                    fin.close()
+
+                    fin = open(path, "rt", encoding= "ANSI")
+                    data = fin.read()
+                    data = data.replace("\\", "/")
+                    fin.close()
+
+                    fin = open(path, "wt", encoding= "ANSI")
+                    fin.write(data)
+                    fin.close()
                 
             fin.close()
 
