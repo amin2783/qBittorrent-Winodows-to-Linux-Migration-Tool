3408c4de48aa70f494b53b2663e5e8da100301ee
amin2783 <64882854+amin2783@users.noreply.github.com>
2021-04-18 20:42:36 +0600

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import pathlib
+
+alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for char in alphabet:
+
+    for path in pathlib.Path(r"C:\Users\mamun\Desktop\Python\New folder\\").iterdir():
+        if path.is_file():
+            if "desktop.ini" in str(path):
+                break
+
+
+            fin = open(path, "rt", encoding= "ANSI")
+
+            data = fin.read()
+
+            if f"{char}:\\" in data:
+
+                data = data.replace(f"{char}:\\", f"/home/user/media/{char}:/")
+                fin.close()
+
+                fin = open(path, "wt", encoding= "ANSI")
+                fin.write(data)
+                fin.close()
+
+                fin = open(path, "rt", encoding= "ANSI")
+                data = fin.read()
+                data = data.replace("\\", "/")
+                fin.close()
+
+                fin = open(path, "wt", encoding= "ANSI")
+                fin.write(data)
+                fin.close()
+                
+            fin.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
